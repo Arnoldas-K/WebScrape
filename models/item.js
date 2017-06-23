0adbe8884973782646e8337097f12379ab37552d
Arnoldas-K <arnoldask.96@gmail.com>
2017-06-23 17:22:27 +0100

    Small fixes in "grindlondon" shop scraper.

--- models/item.js
+++ models/item.js
@@ -3,16 +3,16 @@ var Schema = mongoose.Schema;
 
 function dynamicScheme(nameOfCollection) {
     var ItemSchema = new Schema({
-        sex: {type: String, required: true},
-        season: {type: String, required: true},
-        type: {type: String, required: true},
-        name: {type: String, required: true},
+        sex: {type: String},
+        season: {type: String},
+        type: {type: String},
+        name: {type: String},
         category: {type: [String], default: ''},
-        price: { type: String, required: true},
-        currency: { type: String, required: true},
-        cover: {type: String, required: true},
+        price: { type: String},
+        currency: { type: String},
+        cover: {type: String},
         hover: {type: String, default: ''},
-        url: {type: String, required: true},
+        url: {type: String},
         sizes: {type: [String], default: ''}
     });
 //module.exports = mongoose.model('Item', ItemSchema);
